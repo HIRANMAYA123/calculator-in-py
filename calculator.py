a2356f66bdc7c579c9dce7ca987aac30452fb6cd
Hiranmaya Padhi <padhihiranmaya@gmail.com>
2025-11-23 02:20:07 +0530

    added operations
    Added a loop to continuously prompt for calculations until interrupted.

--- calculator.py
+++ calculator.py
@@ -4,3 +4,27 @@ print("1-Add")
 print("2-Subtract")
 print("3-Multiply")
 print("4-Divide")
+while True:
+    choice=input("Enter choice 1/2/3/4:")
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1=float(input("Enter first number-"))
+            num2=float(input("Enter second number-"))
+        except ValueError:
+            print("Invalid input")
+            continue
+        if choice=='1':
+            result=num1+num2
+            print(f"{num1}+{num2}={result}")
+        elif choice=='2':
+            result=num1-num2
+            print(f"{num1}-{num2}={result}")
+        elif choice=='3':
+            result=num1*num2
+            print(f"{num1}*{num2}={result}")
+        elif choice=='4':
+            if num2==0:
+                print("error")
+            else:
+                result=num1/num2
+                print(f"{num1}/{num2}={result}")
